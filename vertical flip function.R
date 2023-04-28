09cd33d51f52d49947f57f6a1182d489ef1bbb31
gmarzen <83039280+gmarzen@users.noreply.github.com>
2023-04-28 12:52:48 -0400

    Update vertical flip function.R

--- vertical flip function.R
+++ vertical flip function.R
@@ -4,27 +4,25 @@ library(tensorflow)
 library(reticulate)
 library(magick)
 library(jpeg)
-reticulate:: py_install("pillow", force = TRUE)
+reticulate::py_install("pillow", force = TRUE)
 
-path <- "dandelions"
-filelist <- list.files(path, pattern = ".jpg", full.names = TRUE)
-
-vertical_flip <- function(files)
-for (file in filelist) {
-  img <- image_read(file)
-  vert_flip <- image_flip(img)
-  
-  new_file <- sub(".jpg", "_vertflip.jpg", file)
-  image_write(vert_flip, new_file)
+vertical_flip <- function(filelist) {
+ 
+  # for loop to run through each image
+  for (file in filelist) {
+    img <- image_read(file)
+    vert_flip <- image_flip(img)
+    
+    #create the new image and save
+    new_file <- sub(".jpg", "_vertflip.jpg", file)
+    image_write(vert_flip, new_file)
+  }
 }
 
-path2 <- "grass"
-filelist2 <- list.files(path2, pattern = ".jpg", full.names = TRUE)
+# dandelion picture change
+dandelionPath <- list.files("dandelion", pattern = "\\.jpg$", full.names = TRUE)
+vertical_flip(dandelionPath)
 
-for(file in filelist2){
-  img2 <- image_read(file)
-  vert_flip2 <- image_flip(img2)
-  
-  new_file2 <- sub(".jpg", "_vertflip.jpg", file)
-  image_write(vert_flip2, new_file2)
-}
+# grass picture change
+grassPath <- list.files("grass", pattern = "\\.jpg$", full.names = TRUE)
+vertical_flip(grassPath)
