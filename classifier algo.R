a68e3cf1fafa9980a5a2c7d1d8ded83a72c44812
belldm <123672272+belldm@users.noreply.github.com>
2023-04-28 18:00:28 -0400

    Add files via upload

--- classifier algo.R
+++ classifier algo.R
@@ -1,18 +1,31 @@
+install.packages("keras")
 library(keras)
-res1=c("","")
-f=list.files("./grass")
+library(reticulate)
+reticulate:: py_install("pillow", force = TRUE)
+
+
+res1 <- c("", "")
+f <- list.files("./grass")
 target_size <- c(224, 224)
-for (w in f){
-  test_image <- image_load(paste("./grass/",w,sep=""),
+
+# Load the model
+model <- load_model_hdf5("dandelion_model")
+
+for (w in f) {
+  test_image <- image_load(paste("./grass/", w, sep=""),
                            target_size = target_size)
   x <- image_to_array(test_image)
   x <- array_reshape(x, c(1, dim(x)))
   x <- x/255
+  
+  # Use the standard pipe operator
   pred <- model %>% predict(x)
-  if(pred[1,2]<0.50){
+  
+  if (pred[1, 1] < 0.50) {
     print(w)
   }
 }
+
 print(res1)
 
 res2=c("","")
