b3c529f484f041107668a8aed3087f949dd61e61
gmarzen <83039280+gmarzen@users.noreply.github.com>
2023-04-28 12:47:14 -0400

    Update isoblur function.R

--- isoblur function.R
+++ isoblur function.R
@@ -1,21 +1,25 @@
 library(imager)
 
-# Set the path to your image folder
-img_folder <- "C:/Users/giann/Documents/ie332/dandelions"
-
-# Loop over all files in the folder with the .jpg extension
-for (file in list.files(img_folder, pattern = "\\.jpg$")) {
-  
-  # Read in the image
-  img_int <- imager::load.image(file.path(img_folder, file))
-  
-  # Apply Gaussian blur with a radius of 5 pixels
-  blurred_img_int <- imager::isoblur(img, sigma = .01)
-  
-  # Create a new filename for the blurred image
-  new_file <- gsub("\\.jpg$", "_blurred.jpg", file)
-  
-  # Write the blurred image to a new file
-  imager::save.image(blurred_img, file.path(img_folder, new_file))
-  
+isoblurFunction <- function(files, folder_path) {
+  # Loop over all files in the folder with the .jpg extension
+  for (file in files) {
+    
+    # read in the image
+    img_int <- imager::load.image(file.path(folder_path, file))
+    
+    # apply blur, radius = 5 pixels
+    blurred_img_int <- imager::isoblur(img_int, sigma = 5)
+    
+    # create new filename and save blurred image
+    new_file <- gsub("\\.jpg$", "_blurred.jpg", file)
+    imager::save.image(blurred_img_int, file.path(folder_path, new_file))
+  }
 }
+
+# change dandelion pictures
+dandelionPath <- list.files("dandelions", pattern = "\\.jpg$")
+isoblurFunction(dandelionPath, "dandelions")
+
+# change grass pictures
+grassPath <- list.files("grass", pattern = "\\.jpg$")
+isoblurFunction(grassPath, "grass")
